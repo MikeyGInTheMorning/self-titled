680b75f67fdf6eab662fb1a1556f01070fd549a9
Michael <mikegulik@gmail.com>
2022-07-09 16:24:37 -0400

    added pwa for sure this time

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,56 @@
 <script lang="ts">
-  import { onMount } from "svelte"
+  import { onMount } from "svelte";
+  import { registerSW } from "virtual:pwa-register";
 
   onMount(() => {
-    const html = document.documentElement
+    registerSW();
+
+    const html = document.documentElement;
     const canvas = document.getElementById(
       "hero-lightpass"
-    ) as HTMLCanvasElement
-    const context = canvas.getContext("2d")
+    ) as HTMLCanvasElement;
+    const context = canvas.getContext("2d");
 
-    const frameCount = 148
+    const frameCount = 147;
     const currentFrame = (index) =>
       `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
         .toString()
-        .padStart(4, "0")}.jpg`
+        .padStart(4, "0")}.jpg`;
 
     const preloadImages = () => {
       for (let i = 1; i < frameCount; i++) {
-        const img = new Image()
-        img.src = currentFrame(i)
+        const img = new Image();
+        img.src = currentFrame(i);
       }
-    }
+    };
 
-    const img = new Image()
-    img.src = currentFrame(1)
-    canvas.width = html.offsetWidth
-    canvas.height = html.offsetHeight
+    const img = new Image();
+    img.src = currentFrame(1);
+    canvas.width = html.offsetWidth;
+    canvas.height = html.offsetHeight;
     img.onload = function () {
-      context.drawImage(img, 0, 0)
-    }
+      context.drawImage(img, 0, 0);
+    };
 
     const updateImage = (index) => {
-      img.src = currentFrame(index)
-      context.drawImage(img, 0, 0)
-    }
+      img.src = currentFrame(index);
+      context.drawImage(img, 0, 0);
+    };
 
     window.addEventListener("scroll", () => {
-      const scrollTop = html.scrollTop
-      const maxScrollTop = html.scrollHeight - window.innerHeight
-      const scrollFraction = scrollTop / maxScrollTop
+      const scrollTop = html.scrollTop;
+      const maxScrollTop = html.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollTop / maxScrollTop;
       const frameIndex = Math.min(
         frameCount - 1,
         Math.ceil(scrollFraction * frameCount)
-      )
+      );
 
-      requestAnimationFrame(() => updateImage(frameIndex + 1))
-    })
+      requestAnimationFrame(() => updateImage(frameIndex + 1));
+    });
 
-    preloadImages()
-  })
+    preloadImages();
+  });
 </script>
 
 <canvas id="hero-lightpass" />
@@ -60,16 +63,11 @@
     background: #000;
     height: 500vh;
   }
-  :global(.app) {
-    background: #000;
-    width: 100%;
-  }
   canvas {
     position: fixed;
-    left: calc(100vw/5);
-    top: calc(100vw/4);
+    left: calc(100vw / 5);
+    top: calc(100vw / 4);
     max-height: 100vh;
     max-width: 100vw;
-
   }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,47 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePWA } from 'vite-plugin-pwa'
+import { ConfigEnv, defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const pwaOptions = (a: ConfigEnv): Partial<VitePWAOptions> => ({
+  registerType: "autoUpdate",
+  mode: a.mode === "production" ? "production" : "development",
+  includeAssets: ["favicon.svg"],
+  workbox: {
+    clientsClaim: true,
+    skipWaiting: true,
+  },
+  manifest: {
+    name: "PWA Router",
+    short_name: "PWA Router",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "pwa-192x192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/pwa-512x512.png", // <== don't remove slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "pwa-512x512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+  devOptions: {
+    enabled: true,
+    /* when using generateSW the PWA plugin will switch to classic */
+    type: "module",
+    navigateFallback: "index.html",
+  },
+});
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte(), VitePWA({
-    registerType: 'autoUpdate',
-    devOptions: {
-      enabled: true
-    }
-  })]
-})
+export default defineConfig((a) => ({
+  plugins: [svelte(), VitePWA(pwaOptions(a))],
+}));
