0b56b90900a1718b737c2f60a5d1bf8c0dbd78e5
Michael <mikegulik@gmail.com>
2022-07-10 02:25:08 -0400

    working on name transition

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,41 @@
   import { onMount } from "svelte";
   import { registerSW } from "virtual:pwa-register";
 
+  let titleStyle;
   let scrollY;
+
+  const setTitleStyle = (opacity) => {
+    titleStyle = `--title-opacity: ${opacity};`;
+  };
+
+  const setTitle = (index) => {
+    const titleYStart = 17;
+    const titleYEnd = 65;
+    const frameLengths = 10;
+
+    if (index >= titleYStart - frameLengths && index < titleYStart)
+    {
+      const opacity = (titleYStart - index) / frameLengths
+      console.log(opacity)
+      setTitleStyle(opacity);
+    }
+    else if(index <= titleYEnd + frameLengths && index > titleYEnd)
+    { 
+      console.log("this is outro")
+      setTitleStyle((titleYEnd - index) / frameLengths / 100);
+    }
+    else if(index >= titleYStart && index <=titleYEnd)
+    {
+      console.log("this is 1")
+      setTitleStyle(1)
+    }
+      else{
+        console.log("this is 0")
+        setTitleStyle(0)
+      }
+  };
+
+
 
   onMount(() => {
     registerSW();
@@ -13,13 +47,11 @@
     ) as HTMLCanvasElement;
     const context = canvas.getContext("2d");
 
-    let url = `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass`
-    url = './assets/'
+    let url = `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass`;
+    url = "./assets/";
     const frameCount = 77;
     const currentFrame = (index) =>
-      `${url}${index
-        .toString()
-        .padStart(5, "0")}.png`;
+      `${url}${index.toString().padStart(5, "0")}.png`;
 
     const preloadImages = () => {
       for (let i = 1; i < frameCount; i++) {
@@ -30,8 +62,9 @@
 
     const img = new Image();
     img.src = currentFrame(1);
-    canvas.width = html.offsetWidth;
-    canvas.height = html.offsetHeight;
+    canvas.width = html.offsetWidth > 810 ? 810 : html.offsetWidth;
+    canvas.height = html.offsetHeight > 1080 ? 1080 : html.offsetHeight;
+
     img.onload = function () {
       context.drawImage(img, 0, 0);
     };
@@ -44,12 +77,15 @@
     window.addEventListener("scroll", () => {
       const maxScrollTop = html.scrollHeight - window.innerHeight;
       const scrollFraction = scrollY / maxScrollTop;
+
       const frameIndex = Math.min(
         frameCount - 1,
         Math.ceil(scrollFraction * frameCount)
       );
 
+
       requestAnimationFrame(() => updateImage(frameIndex + 1));
+      setTitle(frameIndex + 1)
     });
 
     preloadImages();
@@ -57,24 +93,29 @@
 </script>
 
 <svelte:window bind:scrollY />
-<canvas id="hero-lightpass" />
-<div class="surface">
-  Mike Gulik
+<div class="canvas-container">
+  <canvas id="hero-lightpass" />
+</div>
+<div class="surface" style={titleStyle}>
+  <div class="on-surface title">Mike Gulik.</div>
 </div>
 
 <style>
-  .surface{
+  .title {
+    opacity: var(--title-opacity, 1);
+  }
+  .surface {
     position: fixed;
     top: 0px;
-    left:0px;
-    right:0px;
+    left: 0px;
+    right: 0px;
     bottom: 0px;
 
     display: flex;
     align-items: center;
     justify-content: center;
 
-    background-color: rgba(0, 0, 255, .25);
+    background-color: rgba(0, 0, 255, 0.15);
     color: white;
   }
   :global(html) {
@@ -99,11 +140,21 @@
     border-radius: 20px; /* roundness of the scroll thumb */
     border: 0px solid rgb(175, 175, 175); /* creates padding around scroll thumb */
   }
-  canvas {
+
+  .canvas-container {
     position: fixed;
-    left: calc(100vw / 3);
-    top: 20vh;
-    max-height: 100vh;
-    max-width: 100vw;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    top: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  canvas {
+    width: 810px;
+    max-width: 810px;
   }
 </style>
