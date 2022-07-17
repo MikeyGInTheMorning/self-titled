d4be0d40a75bc1965b77095aa5a87b4ba4014f67
Michael <mikegulik@gmail.com>
2022-07-16 21:04:56 -0400

    added skyline, asked a stackoverfllow q about it

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,12 @@
   import { registerSW } from "virtual:pwa-register";
   import ImageScroller from "./lib/components/ImageScroller.svelte";
 
-  const baseUrl = "https://mg-portfolio.s3.amazonaws.com/github/MikeyGInTheMorning-2022.stl"
-    //"https://viewscreen.githubusercontent.com/view/solid?color_mode=dark&url=https%3a%2f%2fraw.githubusercontent.com/MikeyGInTheMorning";
 
-  let iFrameSrc = () => "/MikeyGInTheMorning";
+  const BASE_EMBED_URL =
+    "https://viewscreen.githubusercontent.com/view/solid?color_mode=dark&url=https%3a%2f%2fraw.githubusercontent.com";
+    
+  const myRepoUrl = `${BASE_EMBED_URL}/MikeyGInTheMorning/self-titled/dev/public/github/MikeyGInTheMorning-2022.stl`;
+
   onMount(() => {
     registerSW();
   });
@@ -17,16 +19,16 @@
   <div class="relative h-500vh">
     <ImageScroller />
   </div>
-  <div class="h-50vh">
-    sadf
+  <div class="w-full z-10">
     <iframe
+      width="50%"
+      height="500rem"
       title="Github Skyline"
       id="mikes-github-skyline"
       frameborder="0"
       scrolling="no"
-      src="https://viewscreen.githubusercontent.com/view/solid?color_mode=dark&url=https%3A%2F%2Fmg-portfolio.s3.amazonaws.com%2Fgithub%2FMikeyGInTheMorning-2022.stl"
+      src={myRepoUrl}
     />
-
   </div>
 
   <div class="bg-yellow">dsf</div>
